0f62856c07b63a37a7e194b6821c8e0daf4411c5
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-15 06:25:26 +0300

    0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending\
+ * order using the Bubble sort algorithm.
+ * @array: array to sort.
+ * @size: size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, tmp = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				tmp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+
+
