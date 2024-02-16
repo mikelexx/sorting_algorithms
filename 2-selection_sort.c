8ff6cbee0952049906723f4603ce02c609ecf828
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-16 13:24:52 +0300

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in\
+ * ascending order using the Selection sort algorithm
+ * @array: array to be sorted.
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min = 0, holder = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (array[min] < array[i])
+		{
+			holder = array[i];
+			array[i] = array[min];
+			array[min] = holder;
+			print_array(array, size);
+		}
+	}
+}
